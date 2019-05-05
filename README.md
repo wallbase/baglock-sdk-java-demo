a883018c94a6906d9fd90fdbd3a00b2aef743dca
wangko27 <wangkun@nuls.io>
2019-05-05 10:22:55 +0800

    Demo 初始化项目

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # baglock-sdk-java-demo
 
 顺丰箱包锁的demo程序
+
+## 一 环境准备
+- 1 安装jar到本地maven仓库,也可以使用deploy方式发布到公司的私有仓库中去。
+```shell
+mvn install:install-file -Dfile=libs/baglock-sdk-java-1.0.0.jar -DgroupId=com.xeiot.baglock -DartifactId=baglock-sdk-java -Dversion=1.0.0 -Dpackaging=jar
+```
+- 2 引入sdk到项目中,在pom.xml中添加依赖
+
+```xml
+ <!--引入箱包锁的java sdk-->
+<dependency>
+    <groupId>com.xeiot.baglock</groupId>
+    <artifactId>baglock-sdk-java</artifactId>
+    <version>1.0.0</version>
+</dependency>
+```
+## 二 执行测试用例
+> 参考BagLockTest
---
+++ src/test/java/com/xeiot/baglock/sdk/BagLockTest.java
@@ -0,0 +1,73 @@
+package com.xeiot.baglock.sdk;
+
+import com.xeiot.baglock.sdk.model.LockResult;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by wangkun23 on 2019/4/28.
+ */
+public class BagLockTest {
+
+    final Logger logger = LoggerFactory.getLogger(BagLockTest.class);
+
+    /**
+     * 测试用appkey和秘钥生成并验证
+     *
+     * @throws DecoderException
+     */
+    @Test
+    public void test() throws DecoderException {
+        String mac = "DC2C26142293";
+        int sn = 1234567890;
+        String userKey = "1234567890abcd";// 最长14个字节
+
+        //1 在安装时生成的箱包锁秘钥(加密和解密使用)
+        byte[] appKeyBuf = BaglockUtils.getAppKey(mac, sn, userKey);
+        logger.info("appKeyKey {}", Hex.encodeHexString(appKeyBuf).toUpperCase());
+
+        //2 在用户扫码后 获取设备mac地址
+        //  调用服务器端生成秘钥返回加密结果,并把加密结果的第一个字节设置为0x02(开锁),最后两个字节设置为crc校验值
+        Integer RootCNT = 1006;
+        Integer dwTimeStamp = 1545264000;
+        String userId = "18938995";// 最长8 个字节
+        byte[] cipherBuf = BaglockUtils.getCipher(RootCNT, dwTimeStamp, userId);
+        logger.info("未加密 {}", Hex.encodeHexString(cipherBuf).toUpperCase());
+
+        int crc = BagLockAESUtils.crc16(0, cipherBuf);
+        byte[] crcBuf = ByteBuffer.allocate(2).putShort((short) crc).array();
+        logger.info("未加密之前CRC {}", Hex.encodeHexString(crcBuf).toUpperCase());
+
+        byte[] encrypt = BagLockAESUtils.encrypt(cipherBuf, appKeyBuf);
+        logger.info("加密结果 {}", Hex.encodeHexString(encrypt).toUpperCase());
+
+
+        byte[] decrypt = BagLockAESUtils.decrypt(encrypt, appKeyBuf);
+        logger.info("解密结果 {}", Hex.encodeHexString(decrypt).toUpperCase());
+
+        byte[] sendData = ByteBuffer.allocate(20)
+                .put((byte) 0x02)
+                .put(encrypt)
+                .put((byte) 0x00)
+                .put(crcBuf)
+                .array();
+        logger.info("最终发送的秘钥 {}", Hex.encodeHexString(sendData).toUpperCase());
+        // 3 微信小程序收到该秘钥后 执行开锁
+    }
+
+    /**
+     * 解析日志上传
+     * @throws DecoderException
+     */
+    @Test
+    public void parseLog() throws DecoderException {
+        String data = "82000c5c499602d200000000000000000000e0f2";
+        LockResult lockResult = BaglockUtils.parseLockResult(data);
+        logger.info("lockResult {}", lockResult);
+    }
+}
